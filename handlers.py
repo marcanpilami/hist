5e4c632f3be8012a9493ff0618a0df1ffa911312
marcanpilami <marsu_pilami@msn.com>
2009-01-03 15:27:39 +0100

    code reorganisation

--- handlers.py
+++ handlers.py
@@ -19,6 +19,7 @@ from django.db.models.signals import pre_save
 from exceptions import *
 from models import Essence
 from helpers import ModelDiff
+from helpers_copy import _copy_object
 
 
 def __instance_should_be_historised(instance):
@@ -40,73 +41,6 @@ def __instance_should_be_historised(instance):
     return True
 
 
-def __copy_object(original, action_code, version, comment):
-    """factorization of common copy code for all signal handlers"""
-    ## Check it is not a "blank" save
-    try:
-        if action_code != 'delete' and not original.modifications.is_modified():
-            return
-    except:
-        pass # first save
-    
-    ## Create HO object
-    ho = original._meta.history_model(history_action = action_code,
-                                     history_version = version, 
-                                     history_comment = comment,
-                                     history_active_object = original)
-    
-    ## Basic fields
-    for field in original._meta.fields:
-        ## Autofields
-        if field.name == 'id':
-            setattr(ho, 'history_id', getattr(original, field.name))
-        ## FK
-        elif field.__class__.__name__ == 'ForeignKey':
-            try:
-                if getattr(original, field.name)._meta.history_model:
-                    setattr(ho, field.name, getattr(original, field.name).essence)
-                    continue
-                else:
-                    continue ## The target object is not (yet) historised
-            except AttributeError:
-                continue
-        ## Casual fields
-        else:
-            setattr(ho, field.name, getattr(original, field.name))
-    
-    ## Essence
-    try:
-        ho.essence = original.current_avatar.essence
-    except:
-        e = Essence()
-        e.save()
-        ho.essence = e
-    
-    ## M2M
-    ho.save()    ## needed for M2M...
-    for mm in original._meta.many_to_many:
-        try: 
-            # Try/catch is to check whether the target model is historized or not (or if there is nothing to historise)
-            if getattr(original, mm.name).all()[0]._meta.history_model:
-                print 'ici'
-                print getattr(original, mm.name).all() 
-                m2m_manager = getattr(ho, mm.name)
-                for essence in [ ess.current_avatar.essence for ess in getattr(original, mm.name).all() ]:
-                    print essence
-                    m2m_manager.add(essence)
-                print getattr(ho, mm.name).all()
-                print 'fin'
-                continue
-            else:
-                continue ## The target object is not (yet) historised
-        except (AttributeError, IndexError):
-            continue
-    
-    ## End
-    ho.save()
-    return ho
-
-
 def after_save_event_handler(sender, instance, **kwargs):
     """Signal handler for creations"""
     ## Check this object really should be followed by hist
@@ -118,7 +52,7 @@ def after_save_event_handler(sender, instance, **kwargs):
     try:
         instance.current_version
     except:
-        __copy_object(instance, 'creation', 0, u'création de l\'objet')
+        _copy_object(instance, 'creation', 0, u'création de l\'objet')
 
 
 
@@ -143,7 +77,7 @@ def before_save_event_handler(sender, instance, **kwargs):
     version = instance.current_version + 1
     
     ## Create a new history object with the data
-    __copy_object(instance, 'update', version, u'mise à jour sans commentaire')
+    _copy_object(instance, 'update', version, u'mise à jour sans commentaire')
 
 
 
